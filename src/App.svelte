7f80e755332faae811734d905136cb87b54dff65
JoshMBN <brenjm3@student.op.ac.nz>
2022-09-30 12:30:11 +1300

    Added a Fetch script for the website to fetch data from the NIWA Tide API and display it in a large list down the middle of the screen.

--- src/App.svelte
+++ src/App.svelte
@@ -5,53 +5,9 @@
 </script>
 
 <main>
-  <div class="appBlocks">
-    <div class="block one"><TidalStats /></div>
-    <div class="block two"></div>
-    <div class="block three"></div>
-    <div class="block four"></div>
-  </div>
+
 </main>
 
 <style>
-  .appBlocks {
-    display: grid;
-    gap: 20px;
-    background-color: gray;
-
-  }
-  .block {
-    width: 600px;
-    height: 300px;
-    border: 2px solid black;
-  }
-  .one {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    background-color: aquamarine;
-    color: black;
-  }
-  .two {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 1;
-    grid-row-end: 2;
-    background-color: darkslategray;
-  }
-  .three {
-    grid-column-start: 1;
-    grid-column-end: 2;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    background-color: bisque;
-  }
-  .four {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row-start: 2;
-    grid-row-end: 3;
-    background-color: darkkhaki;
-  }
+  
 </style>
--- src/lib/TidalStats.svelte
+++ src/lib/TidalStats.svelte
@@ -1,12 +1,35 @@
-<!-- Demo code sample. Not indended for production use. -->
 <script>
+// Tidal Fetch API.
+
+  const url = "https://api.niwa.co.nz/tides/data?lat=-45.875&long=170.509&numberOfDays=7&apikey=Zqq1PDtLanleKh2fMvYaDGU0FZAJokWJ";
+  const options = {
+    method: "GET",
+    headers: {
+      "x-apikey": "Zqq1PDtLanleKh2fMvYaDGU0FZAJokWJ",
+      "Accept": "application/json"
+    },
+  };
+  let tide_list = document.createElement("ul");
+
+  fetch(url)
+  .then(response => response.json())
+    .then(values => {
+    values["values"].forEach(tide => {
+        let item = document.createElement("li");
+        item.innerHTML = "Date/Time: " + tide.time + " Tide Height: " + tide.value + "m";
+        let testDate = new Date(tide.time) //Date Object
+        console.log(testDate.toString()) //Converts to Local Time Zone
+
+        console.log(tide.value)
+        tide_list.append(item);
+      
+    });
+    document.querySelector("body").append(tide_list);
+
+});
 
 </script>
 
-<h1>Tidal Information</h1>
-
 <style>
-    h1 {
-        padding-top: 80px;
-    }
+    
 </style>
