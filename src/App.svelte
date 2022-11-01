06ee50c2dcf0afaa25b4e8ed95c54af3f3c0b741
zilazach <105255994+zilazach@users.noreply.github.com>
2022-11-01 16:03:55 +1300

    added two more accordions

--- src/App.svelte
+++ src/App.svelte
@@ -14,6 +14,30 @@
       </p>
     </div>
   </Accordion>
+
+  <Accordion>
+    <span slot="head">tide</span>
+    <div slot="details">
+      <p>
+        These are the second details.
+      </p>
+      <p>
+        This can naturally span multiple lines.
+      </p>
+    </div>
+  </Accordion>
+
+  <Accordion>
+    <span slot="head">condition</span>
+    <div slot="details">
+      <p>
+        These are the second details.
+      </p>
+      <p>
+        This can naturally span multiple lines.
+      </p>
+    </div>
+  </Accordion>
 </main>
 
 <style>
