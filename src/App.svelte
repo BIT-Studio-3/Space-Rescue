24074c07f460212375dce739c62243671b729cc1
zilazach <105255994+zilazach@users.noreply.github.com>
2022-11-08 11:26:30 +1300

    Sorted layout of the app. By puting the components in to the accordion.

--- src/App.svelte
+++ src/App.svelte
@@ -16,32 +16,22 @@
   <Location bind:latitude={lat} bind:longitude={long} bind:key={importKey}/>
 
   {#key temp}
-  <TidalStats lat={lat} long={long}/>
-  <Weather exportKey={importKey}/>
-  {/key}
+  
+  
+  
 
   <!--each accordion is a dropdown section that will contain individual components-->
   <Accordion>
     <span slot="head">weather</span>
     <div slot="details">
-      <p>
-        These are the second details.
-      </p>
-      <p>
-        This can naturally span multiple lines.
-      </p>
+      <Weather exportKey={importKey}/>
     </div>
   </Accordion>
 
   <Accordion>
     <span slot="head">tide</span>
     <div slot="details">
-      <p>
-        These are the second details.
-      </p>
-      <p>
-        This can naturally span multiple lines.
-      </p>
+      <TidalStats lat={lat} long={long}/>
     </div>
   </Accordion>
 
@@ -56,7 +46,7 @@
       </p>
     </div>
   </Accordion>
-
+  {/key}
 </main>
 
 
