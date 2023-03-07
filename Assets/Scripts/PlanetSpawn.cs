0979d14201bf252c68603afe722da7e5e96cbec5
Erika Stuart <erikat.stuart@gmail.com>
2023-03-07 21:16:13 +1300

    Planets spawn randomly and not in a line

--- Assets/Scripts/PlanetSpawn.cs
+++ Assets/Scripts/PlanetSpawn.cs
@@ -12,32 +12,39 @@ using UnityEngine;
 public class PlanetSpawn : MonoBehaviour
 {
     //variables
-    public GameObject planetPrefab; //the planet prefab
+    public GameObject planetPrefab; //the planet prefab - used for manager gameobject
     //public Transform planetSpawnPoint;
-    private int spawnRange;
+    private int spawnX;
+    private int spawnY;
+    private int spawnZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawningPlanet();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        for (int i = 0; i < 5; i++)
         {
             SpawningPlanet();
         }
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    SpawningPlanet();
+        //}
+    }
+
     public void SpawningPlanet()
     {
         //Destroy(planetPrefab);
-        for (int i = 0; i<3; i++)
-        {
-            spawnRange = Random.Range(-10, 11);
-        }
-        Instantiate(planetPrefab, new Vector3(spawnRange, spawnRange, spawnRange), Quaternion.identity);
+        //for (int i = 0; i<3; i++)
+        //{
+        spawnX = Random.Range(-10, 11);
+        spawnY = Random.Range(1, 11); //temporarily Y:1+ so planets don't spawn inside or  
+        spawnZ = Random.Range(-10, 11);
+        //}
+        Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
     }
 }
