e5ee6327bc918da58adc90563a84b5aa73173888
erokasyuart <erikat.stuart@gmail.com>
2023-03-22 11:56:15 +1300

    Initial distance changes made; Debugging with debug.log and crashes unity

--- Assets/Scripts/PlanetSpawn.cs
+++ Assets/Scripts/PlanetSpawn.cs
@@ -2,12 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* To Do
- * + When the game starts, spawn planet prefab 
- * + Planet position should be random on spawn (In a range)
- * + Random scale
- * + Many planets should exist on spawn 
- */
 public class PlanetSpawn : MonoBehaviour
 {
     //variables
@@ -21,8 +15,8 @@ public class PlanetSpawn : MonoBehaviour
     public List<GameObject> planets = new List<GameObject>();
 
     //inspector editable variables
-    public int planetCount;
-    public int spawnRange;
+    public int spawnRange = 100;
+    public int spawnCount = 10;
     /*
     I use names here for comparing different planets from each other (I was going to use this for scale but I didn't)
     I could have created a tag and added it to each prefab but we might need the tag space later on
@@ -32,23 +26,88 @@ public class PlanetSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < planetCount; i++)
-        {
-            planets.Add(SpawningPlanet());
-            //SpawningPlanet();
-            //planetTemp.name = planetNames[i]; //names are removed as they are hardcoded
-        }
+        SpawningPlanet();
     }
 
-    //is called each iteration of that for loop so this describes ONE planet
-    private GameObject SpawningPlanet()
-    {   
-        spawnX = Random.Range(-spawnRange, spawnRange);
-        spawnY = Random.Range(1, spawnRange); //temporarily Y:1+ so planets don't spawn inside or under the plane
-        spawnZ = Random.Range(-spawnRange, spawnRange);
-        randScale = Random.Range(5, 51);
-        planetTemp = Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
-        planetTemp.transform.localScale = new Vector3(randScale, randScale, randScale);
-        return planetTemp;
+    private void SpawningPlanet()
+    {
+        //however many x number of planets
+        for (int i = 0; i < spawnCount; i++)
+        {
+            //does the random spawn stuff
+            spawnX = Random.Range(-spawnRange, spawnRange);
+            spawnY = Random.Range(1, spawnRange); //temporarily Y:1+ so planets don't spawn inside or under the plane
+            spawnZ = Random.Range(-spawnRange, spawnRange);
+            randScale = Random.Range(10, 51);
+
+            //if the list isn't empty
+            if (planets.Count != 0)
+            {
+                //Debug.Log("Spawning else"); works
+                //then it checks through whatever IS in the list
+                for (int j = 0; j < planets.Count; j++)
+                {
+                    Debug.Log("j for loop"); //works w/o planets.count -1
+                    //if the current spawning planet is intersecting any planet in the list (+50 for some room)
+                    //if (planets[i].transform.position.x <= planets[j].transform.position.x)
+                    //{
+                    //    Debug.Log("extends range");
+                    //    //then extend it out a bit
+                    //    spawnX += 100;
+                    //}
+                    //else
+                    //{
+                        Debug.Log("Spawning List");
+                        planetTemp = Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+                        planetTemp.transform.localScale = new Vector3(randScale, randScale, randScale);
+                        planets.Add(planetTemp);
+                    //}
+                    
+                }
+            }
+            //if the list is empty
+            else
+            {
+                Debug.Log("Intitial List Spawn");
+                spawnX = Random.Range(-spawnRange, spawnRange);
+                spawnY = Random.Range(1, spawnRange); //temporarily Y:1+ so planets don't spawn inside or under the plane
+                spawnZ = Random.Range(-spawnRange, spawnRange);
+                randScale = Random.Range(10, 51);
+                planetTemp = Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+                planetTemp.transform.localScale = new Vector3(randScale, randScale, randScale);
+                planets.Add(planetTemp);
+            }
+
+        }
+
+        /*
+        //spawns x number of planets
+        for (int i = 0; i < spawnCount; i++)
+        {
+            //randomised spawn
+            spawnX = Random.Range(-spawnRange, spawnRange);
+            spawnY = Random.Range(1, spawnRange); //temporarily Y:1+ so planets don't spawn inside or under the plane
+            spawnZ = Random.Range(-spawnRange, spawnRange);
+            randScale = Random.Range(5, 51);
+
+            //runs through the list of spawned planets
+            for (int j = 0; j < planets.Count - 1; j++)
+            {
+                if (planets[i].transform.position.x <= planets[j].transform.position.x + 50)
+                {
+                    spawnX += 100;
+                }
+                else
+                {
+                    planets.Add(Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity));
+                    planetTemp.transform.localScale = new Vector3(randScale, randScale, randScale);
+                }
+
+            }
+            //planetTemp = Instantiate(planetPrefab, new Vector3(spawnX, spawnY, spawnZ), Quaternion.identity);
+            
+        }
+        */
+        
     }
 }
