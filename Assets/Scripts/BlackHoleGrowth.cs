c6edf3cddf3d2271884b1e480721690683740d29
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2023-03-22 12:52:20 +1300

    Added expanding sphere for black hole

---
+++ Assets/Scripts/BlackHoleGrowth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleGrowth : MonoBehaviour
+{
+    public float targetScale; 
+    public float timeToReachTarget; 
+    private float startScale;  
+    private float percentScaled; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (percentScaled < 1f) 
+        {
+            percentScaled += Time.deltaTime / timeToReachTarget; 
+            float scale = Mathf.Lerp(startScale, targetScale, percentScaled); 
+            transform.localScale = new Vector3(scale, scale, scale); 
+        }
+    }
+}
