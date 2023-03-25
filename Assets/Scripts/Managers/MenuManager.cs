dbdb9b7d4888a5ca927f3d9998822345814d0e8f
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-03-26 12:42:59 +1300

    Added Controls List to settings screen

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -7,28 +7,17 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Canvas myCanvas;
-    public Slider mySlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (myCanvas != null)
-        {
-            myCanvas.scaleFactor = GameSettings.menuScale; //
-        }
-        if (mySlider != null)
-        {
-            mySlider.value = GameSettings.menuScale;
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mySlider != null)
-        {
-        GameSettings.menuScale = mySlider.value;
-        }
+
     }
 
     public void PlayButton()
