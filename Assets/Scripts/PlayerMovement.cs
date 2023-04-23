f82b3d9c127c67e50124d06098c89c24528b56d9
Erika Stuart <erikat.stuart@gmail.com>
2023-04-24 11:26:22 +1200

    Added a speed limit to prevent over acceleration; Fixed player being able to fly into the air

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed;
 
-    public Transform orientation;
+    public float groundDrag;
 
+    [Header("Ground Check")]//needs tweaking!
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
     float horizontalInput;
     float verticalInput;
-
     Vector3 moveDirection;
-
     Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +27,21 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
+    void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //checks if the player is contacting the ground using ~raycast~ o_O
+        UserInput();
+        SpeedControl();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
     // Update is called once per frame
     private void FixedUpdate()
     {
@@ -36,5 +57,17 @@ public class PlayerMovement : MonoBehaviour
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection * moveSpeed * 10, ForceMode.Force);
+    }
+
+    //Limits the acceleration
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        
+        if (flatVel.magnitude > moveSpeed)//if faster than movement speed
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;//calculate the max speed
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);//apply that max speed limit
+        }
     }
 }
