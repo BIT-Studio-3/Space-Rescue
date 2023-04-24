c2ea186364a1e605ec4bdc36bb9bc6e51bbe40b8
Chase Bennett-Hill (1000106289) <BENNC9@student.op.ac.nz>
2023-04-24 16:21:13 +1200

    Addressed requested changes, added limit to distance indicator and removed outdated code. renamed variables for clarity

--- Assets/Scripts/FindingPlanets.cs
+++ Assets/Scripts/FindingPlanets.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class FindingPlanets : MonoBehaviour
 {
-    public  List<GameObject> planetsNotRescued;
+    public List<GameObject> planetsNotRescued;
     public GameObject player;
     public Text distanceText;
 
@@ -14,19 +14,32 @@ public class FindingPlanets : MonoBehaviour
     // Start is called before the first frame update
 
 
-public Camera cam;  //Camera to use
-GameObject target; //Target to point at (you could set this to any gameObject dynamically)
-private Vector3 targetPos; //Target position on screen
-private Vector3 screenMiddle; //Middle of the screen
+    public Camera cam;  //Camera to use
+    private Vector3 targetPos; //Target position on screen
+    private Vector3 screenMiddle; //Middle of the screen
 
+    void Start()
+    {
+        distanceText.text = "";
+        GameObject[] allPlanets = GameObject.FindGameObjectsWithTag("Planet"); //have to do this inside  update for now
+        print(allPlanets.Length);
 
-void Update() { //2d arrow (working)
+        foreach (GameObject planet in allPlanets)
+        {
+            if (!planet.GetComponent<PlanetDetection>().planetRescued)
+            {
+                planetsNotRescued.Add(planet);
+            }
+        }
+    }
+    void Update()
+    { 
 
-            GameObject closestPlanet = null;
+        GameObject closestPlanet = null;
         float minDist = Mathf.Infinity;
-        Vector3 currentPos = player.transform.position; 
+        Vector3 currentPos = player.transform.position;
 
-        foreach(GameObject planet in planetsNotRescued)
+        foreach (GameObject planet in planetsNotRescued) //finds the closest planet to the player that hasn't already been rescued.
         {
             Vector3 directionToTarget = planet.transform.position - currentPos;
             float dist = directionToTarget.sqrMagnitude;
@@ -34,108 +47,62 @@ void Update() { //2d arrow (working)
             {
                 closestPlanet = planet;
                 minDist = dist;
-                distanceText.text = Mathf.Round(minDist/100).ToString();
+                if(minDist < 3000000) //test if the distance to the closest exceeds the limit
+                {
+                    distanceText.text = Mathf.Round(minDist / 100).ToString(); //round the distance to a whole number for readability 
+                }
+                else 
+                {
+                    distanceText.text = "Out of Range!";
+                }
             }
 
-            if(planet == null || planet.GetComponent<PlanetDetection>().planetRescued == true) 
+            if (planet == null || planet.GetComponent<PlanetDetection>().planetRescued == true)
             {
-                
-                planetsNotRescued.Remove(planet);
+
+                planetsNotRescued.Remove(planet); //Remove saved planets from the list
                 break;
             }
 
         }
-         target = closestPlanet;
-    //Get the targets position on screen into a Vector3
-    targetPos = cam.WorldToScreenPoint (target.transform.position);
-    //Get the middle of the screen into a Vector3
-    screenMiddle = new Vector3(Screen.width/2, Screen.height/2, 0); 
-    crosshair.transform.position = screenMiddle;
-    //Compute the angle from screenMiddle to targetPos
-    float tarAngle = (Mathf.Atan2(targetPos.x-screenMiddle.x,Screen.height-targetPos.y-screenMiddle.y) * Mathf.Rad2Deg)+90;
-    if (tarAngle < 0) tarAngle +=360;
-
-
- //Calculate the angle from the camera to the target
- Vector3 targetDir = target.transform.position - cam.transform.position;
- Vector3 forward = cam.transform.forward;
- float angle = Vector3.Angle(targetDir, forward);
-//  distanceText.text = angle.ToString();
-print(angle);
-if(angle < 10 && angle > 0)
-{
-    GetComponent<Renderer>().enabled = false;
-}
-else
-{
-    GetComponent<Renderer>().enabled = true;
-}
-
- //If the angle exceeds 90deg inverse the rotation to point correctly
- if(angle > 90){
-     transform.localRotation = Quaternion.Euler(-tarAngle,90,270);
- } else {
-     transform.localRotation = Quaternion.Euler(tarAngle,270,90);
- }
-
-}
-    void Start()
-    {
-        distanceText.text = "";
-        GameObject[] allPlanets = GameObject.FindGameObjectsWithTag("Planet"); //have to do this inside  update for now
-        print(allPlanets.Length);
-
-        foreach(GameObject planet in allPlanets)
+        GameObject target = closestPlanet;
+        //Get the targets position on screen into a Vector3
+        targetPos = cam.WorldToScreenPoint(target.transform.position);
+        //Get the middle of the screen into a Vector3
+        screenMiddle = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        crosshair.transform.position = screenMiddle;
+        //Compute the angle from screenMiddle to targetPos
+        float tarAngle = (Mathf.Atan2(targetPos.x - screenMiddle.x, Screen.height - targetPos.y - screenMiddle.y) * Mathf.Rad2Deg) + 90;
+        if (tarAngle < 0)
         {
-            if(planet.GetComponent<PlanetDetection>().planetRescued != true) 
-            {
-                planetsNotRescued.Add(planet);
-            }
+            tarAngle += 360;
         }
 
+        //Calculate the angle from the camera to the target
+        Vector3 targetDir = target.transform.position - cam.transform.position;
+        Vector3 forward = cam.transform.forward;
+        float angle = Vector3.Angle(targetDir, forward);
+        //  distanceText.text = angle.ToString();
+        print(angle);
+        if (angle < 10 && angle > 0) //Range for the crosshair to the planet. This is the value when the arrow will disappear
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+        else
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
 
-        
+        //If the angle exceeds 90deg inverse the rotation to point correctly
+        if (angle > 90)
+        {
+            transform.localRotation = Quaternion.Euler(-tarAngle, 90, 270);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(tarAngle, 270, 90);
+        }
+
     }
 
-    // // Update is called once per frame //3d Arrow (old)
-    // void Update()
-    // {
-
-        // GameObject closestPlanet = null;
-        // float minDist = Mathf.Infinity;
-        // Vector3 currentPos = player.transform.position; 
-
-        // foreach(GameObject planet in planetsNotRescued)
-        // {
-        //     Vector3 directionToTarget = planet.transform.position - currentPos;
-        //     float dist = directionToTarget.sqrMagnitude;
-        //     if (dist < minDist)
-        //     {
-        //         closestPlanet = planet;
-        //         minDist = dist;
-        //         distanceText.text = minDist.ToString();
-        //     }
-
-        //     if(planet == null || planet.GetComponent<PlanetDetection>().planetRescued == true) 
-        //     {
-                
-        //         planetsNotRescued.Remove(planet);
-        //         break;
-        //     }
-
-        // }
-        // GameObject target = closestPlanet;
-    //     Vector3 targetPostition = new Vector3( target.transform.position.x,target.transform.position.y,target.transform.position.z);
-        
-    //   targetPostition.y = 0.0f; //Arrow only roates on the y axis when looking at target
-    //   transform.LookAt( targetPostition );
-
-
-
-    //     //  Quaternion newRotation = Quaternion.LookRotation( target.transform.position-currentPos , Vector3.forward);
-    //     // // newRotation.x = 0.0f;
-    //     // // newRotation.z = 0.0f;
-    //     // transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 8) /* Quaternion.Euler(0,player.transform.rotation.y,0) */;
-        
-    // }
 }
