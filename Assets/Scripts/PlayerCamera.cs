b41d49ae3e0b96084cbb87fe9ccb1d98c3b87a62
Erika Stuart <erikat.stuart@gmail.com>
2023-04-25 21:55:24 +1200

    Fixed player being flipped dramatically when they move

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -13,7 +13,8 @@ public class PlayerCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
