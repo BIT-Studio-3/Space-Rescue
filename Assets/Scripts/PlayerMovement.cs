093a3c70510e8525a7697c1466b383eefdeef818
Erika Stuart <erikat.stuart@gmail.com>
2023-04-27 19:57:45 +1200

    Fixed jittering and randomly moving forward

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     public float smoothTime = 0.1f;
     float smoothVelo;
 
+    public Transform cam;
+
     [Header("Ground Check")]//needs tweaking!
     public float playerHeight;
     public LayerMask whatIsGround;
@@ -32,10 +34,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //checks if the player is contacting the ground using ~raycast~ o_O
-        UserInput();
-        SpeedControl();
-
+        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //checks if the player is contacting the ground using ~raycast~ o_O
+        //UserInput();
+        MovePlayer();
+        //SpeedControl();
+        /*
         if (grounded)
         {
             rb.drag = groundDrag;
@@ -44,9 +47,10 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.drag = 0;
         }
+        */
     }
     // Update is called once per frame
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
         MovePlayer();
     }
@@ -54,25 +58,29 @@ public class PlayerMovement : MonoBehaviour
     private void UserInput(){
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-    }
+    }*/
 
     private void MovePlayer()
     {
-        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        //moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         //rb.AddForce(moveDirection * moveSpeed * 10, ForceMode.Force);
         Vector3 direction = new Vector3 (horizontalInput, 0f, verticalInput).normalized; //only x & z
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelo, smoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            controller.Move(direction * speed * Time.deltaTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; //follows camera
+            controller.Move(moveDir * speed * Time.deltaTime);
         }
     }
 
     //Limits the acceleration
-    private void SpeedControl()
+    /*private void SpeedControl()
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         
@@ -81,5 +89,5 @@ public class PlayerMovement : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized * speed;//calculate the max speed
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);//apply that max speed limit
         }
-    }
+    }*/
 }
