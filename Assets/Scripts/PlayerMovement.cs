01e07e183f388035c30312eb6bcbfdc7e6d6a966
erokasyuart <erikat.stuart@gmail.com>
2023-04-28 10:13:11 +1200

    Removed unused code and scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    //public float moveSpeed;
     public float speed = 5f;
-    public CharacterController controller;
-    public float groundDrag;
     public float smoothTime = 0.1f;
     float smoothVelo;
-
-    public Transform cam;
-
-    [Header("Ground Check")]//needs tweaking!
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
-
-    public Transform orientation;
-    float horizontalInput;
     float verticalInput;
-    Vector3 moveDirection;
+    float horizontalInput;
+
+    [Header("Game Objects")]
+    public CharacterController controller;
+    public Transform cam;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -37,39 +28,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //checks if the player is contacting the ground using ~raycast~ o_O
-        //UserInput();
-        MovePlayer();
-        //SpeedControl();
-        /*
-        if (grounded)
-        {
-            rb.drag = groundDrag;
-        }
-        else
-        {
-            rb.drag = 0;
-        }
-        */
-    }
-    // Update is called once per frame
-    /*private void FixedUpdate()
-    {
         MovePlayer();
     }
-
-    private void UserInput(){
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-    }*/
 
     private void MovePlayer()
     {
-
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        //moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        //rb.AddForce(moveDirection * moveSpeed * 10, ForceMode.Force);
         Vector3 direction = new Vector3 (horizontalInput, 0f, verticalInput).normalized; //only x & z
 
         if (direction.magnitude >= 0.1f)
@@ -82,16 +47,4 @@ public class PlayerMovement : MonoBehaviour
             controller.Move(moveDir * speed * Time.deltaTime);
         }
     }
-
-    //Limits the acceleration
-    /*private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        
-        if (flatVel.magnitude > speed)//if faster than movement speed
-        {
-            Vector3 limitedVel = flatVel.normalized * speed;//calculate the max speed
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);//apply that max speed limit
-        }
-    }*/
 }
