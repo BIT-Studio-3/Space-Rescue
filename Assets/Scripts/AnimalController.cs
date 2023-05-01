546c10436c22cf8a59230f6669fd634574b1933e
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2023-05-01 15:50:01 +1200

    Animal prototype added; Player can now interact with animal when in radius

---
+++ Assets/Scripts/AnimalController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalController : MonoBehaviour
+{
+    public bool entered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (entered == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Interacted");
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entered = true;
+        }
+    }
+}
