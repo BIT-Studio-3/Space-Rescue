37ae532448a7502fbea3848f4c3bfc69d2ffaa00
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2023-05-05 10:56:49 +1200

    BH scripts readded and fixed

--- Assets/Scripts/Black Hole/BlackHoleGrowth.cs
+++ Assets/Scripts/Black Hole/BlackHoleGrowth.cs
@@ -4,34 +4,10 @@ using UnityEngine;
 
 public class BlackHoleGrowth : MonoBehaviour
 {
-    //The target size. The larger the number the larger it will get and the quicker it will expand
-    private float targetScale = 8000; 
-    //The time it takes to get to the size. The larger number the slower it will expand
-    private float timeToReachTarget = 1200; 
-    //The scale of the object when it starts growing
-    private float startScale;  
-    //The percent of the way to the target it has been scaled
-    private float percentScaled;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Setting the current scale
-        startScale = transform.localScale.x;
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //If it isn't fully scaled
-        if (percentScaled < 1f) 
-        {
-            //Weird thing with keeping a consistant time even if the frame rate fluctuates
-            percentScaled += Time.deltaTime / timeToReachTarget; 
-            //Finding the new scale
-            float scale = Mathf.Lerp(startScale, targetScale, percentScaled); 
-            //Setting the new size
-            transform.localScale = new Vector3(scale, scale, scale); 
-        }
+        //Increases every frame
+        transform.localScale = new Vector3(transform.localScale.x * 1.0001f, transform.localScale.y * 1.0001f, transform.localScale.z * 1.0001f); 
     }
 }
