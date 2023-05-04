0fcc7381e0d5041f4da3771b24f0d993b49e1de9
Erika Stuart <erikat.stuart@gmail.com>
2023-05-05 00:09:21 +1200

    Fixed while loop and the animals now move! (I'm crying tears of joy)

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -7,6 +7,7 @@ public class AnimalController : MonoBehaviour
 {
     public bool inRange = false;
     public bool doneMove= false;
+    public float speed = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -41,13 +42,19 @@ public class AnimalController : MonoBehaviour
 
     IEnumerator wait()
     {
-       // doneMove = false;
-        for (int i=0;i<10;i++)
+        for (int i=0;i<10;i++) //avoiding while cos it crashes unity
         {
-            Vector3 pos = new Vector3(Random.Range(-50,50), 0f, Random.Range(-50, 50));
-            transform.position = pos;
-            
-            yield return new WaitForSeconds(Random.Range(4, 11));
+            Vector3 pos = new Vector3(Random.Range(-50,50), 0, Random.Range(-50, 50)); //picks a random position to move to
+
+            //https://forum.unity.com/threads/help-using-coroutine-to-move-game-object-to-position-wait-then-return-to-original-position.1122784/
+            //BLESSED UNITY FORUMS
+            while (transform.position != pos) //while the animal is not at their desired position
+            {
+                transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime); //move them to it!
+                yield return 0; //used to let the engine wait for a frame which breaks an endless broken loop
+            }
+        yield return new WaitForSeconds(Random.Range(4, 11)); //pause for a random time and then go again
         }
     }
+
 }
