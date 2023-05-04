b6bb0fb9242441a4555152e0e3ff505dfe418378
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2023-05-04 15:48:16 +1200

    Animals now move every 5 seconds with temp for loop instead of while

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class AnimalController : MonoBehaviour
 {
     public bool inRange = false;
+    public bool doneMove= false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(wait());
     }
 
     // Update is called once per frame
@@ -35,6 +36,24 @@ public class AnimalController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             inRange = false;
+        }
+    }
+
+    IEnumerator wait()
+    {
+       // doneMove = false;
+        for (int i=0;i<10;i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-50,50), 0f, Random.Range(-50, 50));
+            transform.position = pos;
+            
+            /*if (transform.position == pos)
+            {
+                doneMove = true;
+            }*/
+        //doneMove = false;
+            yield return new WaitForSeconds(5);
+           // doneMove = true;
         }
     }
 }
