90932045a297a3c64ad038e13c12ed8296d3c85c
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2023-05-15 16:09:02 +1200

    Gravity to sphere functional

---
+++ Assets/Scripts/Planet Gravity/GravityAttractor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAttractor : MonoBehaviour
+{
+    public float gravity = -10;
+
+    public void Attract(Transform body)
+    {
+        Vector3 gravityUp = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+        body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/Planet Gravity/GravityBody.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+    public GravityAttractor attractor;
+    private Transform myTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
