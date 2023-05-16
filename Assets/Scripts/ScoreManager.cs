a3d21eb6cb4e6b9a79a015c009e211592e05effb
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-05-16 21:11:19 +1200

    Fixed issue where rescued planets are displayed as destroyed on UI

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -13,11 +13,17 @@ public class ScoreManager : MonoBehaviour
     public Text planetsSaved;
     public List<GameObject> planetUI;
     public List<GameObject> planets;
+    private List<bool> planetStatus;
     // Start is called before the first frame update
     void Start()
     {
         planetUI = GameObject.FindGameObjectsWithTag("PlanetUI").ToList();
         planets = GameObject.Find("PlanetManager").GetComponent<PlanetSpawn>().planets;
+        planetStatus = new List<bool>();
+        for(int i = 0;i < planets.Count; i++ )
+        {
+            planetStatus.Add( planets[i].GetComponent<PlanetDetection>().planetRescued);
+        }
     }
 
     // Update is called once per frame
@@ -27,11 +33,12 @@ public class ScoreManager : MonoBehaviour
         planetsRemaining.text = "Remaining: " + GameObject.Find("arrow").GetComponent<FindingPlanets>().planetsNotRescued.Count.ToString();
         foreach(GameObject p in planets)
         {
-            if(p == null)
+   
+            if(p != null)
             {
-                planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = destroyed;
-            }
-            else if(p.GetComponent<PlanetDetection>().inDanger == true && p.GetComponent<PlanetDetection>().planetRescued != true)
+                planetStatus[planets.IndexOf(p)] = planets[planets.IndexOf(p)].GetComponent<PlanetDetection>().planetRescued;
+
+            if(p.GetComponent<PlanetDetection>().inDanger == true && p.GetComponent<PlanetDetection>().planetRescued != true)
             {
                 planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = danger;
 
@@ -39,8 +46,22 @@ public class ScoreManager : MonoBehaviour
             else if(p.GetComponent<PlanetDetection>().planetRescued == true)
             {
                 planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = rescued;
-
             }
+            }
+            
+            if(p == null && planetStatus[planets.IndexOf(p)] != true)
+            {
+                planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = destroyed;
+            }
+            // else if(p.GetComponent<PlanetDetection>().inDanger == true && p.GetComponent<PlanetDetection>().planetRescued != true)
+            // {
+            //     planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = danger;
+
+            // }
+            // else if(p.GetComponent<PlanetDetection>().planetRescued == true)
+            // {
+            //     planetUI[planets.IndexOf(p)].GetComponent<MeshRenderer>().material = rescued;
+            // }
         }
         
     }
