2e8cc08625c7e7ae86efe6bbb8906ae0ae1794f0
Yousef Altay (1000070390) <ALTAYY1@student.op.ac.nz>
2023-05-22 15:59:30 +1200

    working on making camera follow player

--- Assets/LookAround.cs
+++ Assets/LookAround.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class LookAround : MonoBehaviour
 {
-    public LookAround target;
-    private float speed = 2.0f;
-    private Vector3 point;
+    private Camera cam;
+    private Transform target;
+    private float distanceToTarget = 10;
+
+    private Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
-        point = target.transform.position;
-        transform.LookAt(point);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround (target.transform.position, Vector3.up, speed * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 newPosition = cam.ScreenToViewportPoint(Input.mousePosition);
+            Vector3 direction = previousPosition - newPosition;
+
+            float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+            float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+
+            cam.transform.position = target.position;
+            
+            cam.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+            cam.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World);
+            
+            cam.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+
+            previousPosition = newPosition;
+        }
     }
 }
