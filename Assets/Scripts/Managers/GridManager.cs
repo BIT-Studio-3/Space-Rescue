e8c1bb54b03789ac17a3f4ea138a556517dfc4e0
Yousef Altay (1000070390) <ALTAYY1@student.op.ac.nz>
2023-05-26 11:33:11 +1200

    mini game timer changed from application editor to game build

--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -48,9 +48,9 @@ public class GridManager : MonoBehaviour
         timerText.text = Mathf.Round(timeLeft).ToString();
         if (timeLeft < 0)
         {
-            UnityEditor.EditorApplication.isPlaying = false; //only works in unity editor
+            //UnityEditor.EditorApplication.isPlaying = false; //only works in unity editor
                                                              //to make it work in the actual game not in editor it needs to be changed 
-            //Application.Quit  //for the main game  
+            Application.Quit  //for the main game  
         }
     }
 
