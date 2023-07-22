27b56044328fb3a242e5fd647a98ff6319192bdf
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-07-22 17:12:33 +1200

    Added keybindings shared script

---
+++ Assets/Scripts/Shared/Keybinds.cs
@@ -0,0 +1,10 @@
+//This keeps all of the keybindings in one, easily changable, script. All key controls should be added to here and used in reference.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keybinds
+{
+
+
+}
