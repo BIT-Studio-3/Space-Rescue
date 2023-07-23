c8b54ac53b718384aaaf971eab7deba2a1ef5982
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-07-23 15:55:17 +1200

    Renamed orbit script for clarity

--- Assets/Scripts/Planets/PlanetOrbit.cs
+++ Assets/Scripts/Planets/PlanetOrbit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravitySource : MonoBehaviour
+public class PlanetOrbit : MonoBehaviour
 {
     private float rotationSpeed = 3.0f;
     // Start is called before the first frame update
