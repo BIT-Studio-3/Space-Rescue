e3632eca98c1f446372eebf70efa4e8a51aa4fda
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-07-23 16:45:40 +1200

    Added basic gravity pulling

---
+++ Assets/Scripts/Black Hole/BlackHoleGravity.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleGravity : MonoBehaviour
+{
+    private float planetGravity = .00005f;
+    private float playerGravity = .05f;
+    private float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        radius = GetComponent<SphereCollider>().radius;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.attachedRigidbody)
+        {
+            float gravityIntensity = Vector3.Distance(transform.position, other.transform.position) / radius;
+            float tempGravity;
+            Debug.Log(other.transform.CompareTag("Player"));
+            if (other.transform.CompareTag("Player")) //Checks if it should use the player or planet gravity
+            {
+                tempGravity = playerGravity;
+            }
+            else
+            {
+                tempGravity = planetGravity;
+            }
+            other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * tempGravity * Time.smoothDeltaTime);
+            Debug.DrawRay(other.transform.position, transform.position - other.transform.position); //To show what it's targetting
+        }
+    }
+}
