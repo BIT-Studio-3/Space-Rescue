3763f31c9511cd3e2d1a267912aeabe9f282ee44
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-07-30 12:51:18 +1200

    Improved documentation

--- Assets/Scripts/Black Hole/BlackHoleGravity.cs
+++ Assets/Scripts/Black Hole/BlackHoleGravity.cs
@@ -23,6 +23,8 @@ public class BlackHoleGravity : MonoBehaviour
             if (other.transform.CompareTag("Planet")) //Checks if it should use the player or planet gravity
             {
                 other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * planetGravity * Time.smoothDeltaTime);
+                //The Add Force method works by determining the position of itself and and the planet/ship and multiplying it by the distance to the object, the mass of the object, 
+                //a gravity multiplier depending on if it is a ship or planet, and the time intervals to keep it an even pull.
             }
             else if (!SpeedEffect.Instance.boosters) // If boosters are off. This is so that the gravity can be threatening but also able to escape it with boosters
             {
