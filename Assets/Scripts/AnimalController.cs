5c8a227324e10bb48c8ae42bf1cc786cec143f3f
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2023-08-08 16:24:35 +1200

    Animal only needs to get close to the target point and won't endlessly hop

--- Assets/Scripts/AnimalController.cs
+++ Assets/Scripts/AnimalController.cs
@@ -5,6 +5,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 //using System;
 
 //Script controls each individual animal after spawn
@@ -14,14 +15,15 @@ public class AnimalController : MonoBehaviour
     private float speed = 10;
     private Vector3 velocity = Vector3.zero;
     private Rigidbody rb;
+    private float radius;
 
-    private float RADIUS = 26;
     private const int MINWAIT = 4;
     private const int MAXWAIT = 11;
 
     // Start is called before the first frame update
     void Start()
     {
+        radius = GameObject.Find("Planet").transform.localScale.x / 2;
         rb = GetComponent<Rigidbody>();
         StartCoroutine(wait());
     }
@@ -31,6 +33,7 @@ public class AnimalController : MonoBehaviour
     {
         if (inRange == true && Input.GetKeyDown(Keybinds.Interact))
         {
+            GameSettings.Score++;
             Destroy(gameObject);
         }
     }
@@ -55,22 +58,28 @@ public class AnimalController : MonoBehaviour
     {
         while (true) //while its running (forever)
         {
-            Vector3 pos = Random.onUnitSphere * RADIUS; //picks a random point on the surface of a sphere with the radius
+            Vector3 pos = Random.onUnitSphere * radius; //picks a random point on the surface of a sphere with the radius
             transform.LookAt(pos, transform.position * Time.deltaTime); //makes them face the direction they will move to
 
             //https://forum.unity.com/threads/help-using-coroutine-to-move-game-object-to-position-wait-then-return-to-original-position.1122784/
             //BLESSED UNITY FORUMS
-            while (transform.position != pos) //while the animal is not at their desired position
+            RaycastHit hit;
+
+            while (Vector3.Distance(transform.position, pos) > 1) //while the animal is not at their desired position
             {
-                if (Physics.Raycast(transform.position, pos, out RaycastHit hit, .1f))
-                    {
-                        //if there is a collider in front of the animal, end the movement
-                        break;
-                    }
-                transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime); //move them to it!
+                if (Physics.Raycast(transform.position, pos, out hit, .1f))
+                {
+                    //if there is a collider in front of the animal, end the movement
+                    break;
+                }
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    pos,
+                    speed * Time.deltaTime
+                ); //move them to it!
                 yield return 0; //used to let the engine wait for a frame which breaks an endless broken loop
             }
-        yield return new WaitForSeconds(Random.Range(MINWAIT, MAXWAIT)); //pause for a random time and then go again
+            yield return new WaitForSeconds(Random.Range(MINWAIT, MAXWAIT)); //pause for a random time and then go again
         }
     }
 }
