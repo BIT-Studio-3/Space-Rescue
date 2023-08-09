cf46cfac636b8930a5f1f954b26933e8e1183f8a
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2023-08-10 11:04:54 +1200

    Added editmode scripts and assembly definitions

---
+++ Assets/Tests/EditMode/ShipMovementTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ShipMovementTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void ShipMovementTestSimplePasses()
+    {
+        Assert.AreEqual();
+    }
+
+}
