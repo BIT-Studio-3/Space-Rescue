742db3fad299140ed1b8a3fe0ac5e24507b0866d
Chase Bennett-Hill (1000106289) <BENNC9@student.op.ac.nz>
2023-08-11 14:16:53 +1200

    Fixed ship booster display

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -105,6 +105,7 @@ public class ShipMovement : MonoBehaviour
     {
         spaceshipRB.AddForce(speedMult * verticalMove * spaceshipRB.transform.TransformDirection(Vector3.forward), ForceMode.VelocityChange);
         float currentVelocity = Mathf.Round((spaceshipRB.velocity - initialVelocity).magnitude);
+        currentVelocity = currentVelocity * 5;
         speedText.text = currentVelocity < 10 ? $"0{currentVelocity}" : $"{currentVelocity}";
         spaceshipRB.AddForce(horizontalMove * speedMult * spaceshipRB.transform.TransformDirection(Vector3.right), ForceMode.VelocityChange);
 
