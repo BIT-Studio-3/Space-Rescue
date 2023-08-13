b02d37ba3b41811691bab845acdce53c0be4f896
Erika Stuart <erikat.stuart@gmail.com>
2023-08-13 14:42:39 +1200

    Buttons working again

---
+++ Assets/Tests/EditMode/MovementTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class MovementTest
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void MovementTestSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+}
