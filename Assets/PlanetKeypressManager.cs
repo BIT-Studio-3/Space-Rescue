548d1999864ef317d12dee1ff34a0504c83a0e8a
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2023-08-15 13:21:32 +1200

    Added functional pause menu to Spherical Planet scene

---
+++ Assets/PlanetKeypressManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetKeypressManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pauseMenu = GameObject.Find("Pause Menu");
+        //pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
