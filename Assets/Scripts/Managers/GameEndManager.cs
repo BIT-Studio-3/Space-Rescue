0f9cd3dab0c204aea3c4febb0fa4883b07b8cc8d
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-08-18 14:27:58 +1200

    Add comments and commented out tutorial statements

--- Assets/Scripts/Managers/GameEndManager.cs
+++ Assets/Scripts/Managers/GameEndManager.cs
@@ -1,5 +1,5 @@
 // Description: Handles the End Screen
-// Author: 
+// Author:
 // Last Updated: 18/8/23
 // Last Updated By: Chase Bennett-Hill
 using System.Collections;
@@ -11,13 +11,10 @@ using UnityEngine.UI;
 
 public class GameEndManager : MonoBehaviour
 {
-
     //Game objects for saved numbers and the text above it
-    public GameObject savedTextGO;
-    public GameObject statusTextGO;
-    //Text announcing the result
-    public Text winningText;
-    //The text of the number of planets saved
+    public GameObject savedTextGO; //Animals saved
+    public GameObject statusTextGO; //Win or lose status
+
     void Start()
     {
         //Updates score and winning state when end game state is entered
@@ -26,25 +23,27 @@ public class GameEndManager : MonoBehaviour
         //Enable cursor
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
-        if(GameSettings.Tutorial)
-            GameObject.Find("Restart").GetComponent<Text>().text = "Continue";
- 
+        // if (GameSettings.Tutorial)
+        //     GameObject.Find("Restart").GetComponent<Text>().text = "Continue";
     }
 
     //Sets the score display to the current game score
     public void UpdateScore()
     {
-        savedTextGO.GetComponent<TMPro.TextMeshProUGUI>().text = GameSettings.Score == 1 ? $"You rescued {GameSettings.Score} animal" : $"You rescued {GameSettings.Score} animals";
+        savedTextGO.GetComponent<TMPro.TextMeshProUGUI>().text =
+            GameSettings.Score == 1
+                ? $"You rescued {GameSettings.Score} animal"
+                : $"You rescued {GameSettings.Score} animals";
     }
 
     //Updates the text depending on if you escaped or died
     public void UpdateWinning()
     {
-        if(GameSettings.Tutorial)
-        {
-            winningText.text = "You Completed the Tutorial! \n Press the button below \nto play the game!";
-        } 
-        else if (GameSettings.Winning)
+        // if(GameSettings.Tutorial)
+        // {
+        //     winningText.text = "You Completed the Tutorial! \n Press the button below \nto play the game!";
+        // }
+        if (GameSettings.Winning)
         {
             statusTextGO.GetComponent<TMPro.TextMeshProUGUI>().text = "Mission Successful!";
         }
@@ -62,14 +61,15 @@ public class GameEndManager : MonoBehaviour
         GameSettings.Tutorial = false;
         SceneManager.LoadScene("Main scene");
     }
+
     public void QuitButton()
     {
         Application.Quit();
     }
+
     public void MenuButton()
     {
         GameSettings.Tutorial = false;
         SceneManager.LoadScene("Title Screen");
     }
-
 }
