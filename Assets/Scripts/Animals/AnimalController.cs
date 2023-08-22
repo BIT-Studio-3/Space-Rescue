8454397a692230b1abc52d48454c1a25e90dde21
Palin Wiseman (1000105609) <WISEPB1@student.op.ac.nz>
2023-08-22 15:15:28 +1200

    Add triggers for hostile and scared movement

--- Assets/Scripts/Animals/AnimalController.cs
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -15,6 +15,7 @@ public class AnimalController : MonoBehaviour
     private float radius;
     private bool inRange = false;
     private bool alerted = false;
+    private bool moving;
     private Vector3 pos;
 
     private const int MINWAIT = 4;
@@ -23,7 +24,7 @@ public class AnimalController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        radius = GameObject.Find("Planet").transform.localScale.x / 2;
+        radius = GameObject.Find("Planet").transform.localScale.x / 2 + 1;
         pos = transform.position; //Sets initial movement to the animals spawn point
         StartCoroutine(wait());
     }
@@ -56,11 +57,32 @@ public class AnimalController : MonoBehaviour
         }
     }
 
+    public void Scared(Vector3 playerPos)
+    {
+        //set pos to 100 units in the opposite direction on the sphere from the player
+        pos = playerPos * -1;
+        LookAtMovement(pos);
+        //Set the pos to a certain distance in the opposite direction from the player and set alerted to make sure a new pos is not set
+    }
+
+    public void Hostile(Vector3 playerPos)
+    {
+        pos = playerPos;
+        alerted = true;
+        LookAtMovement(pos);
+        //Set the pos to the player and set alerted to make sure a new pos is not set
+        //Will also need to check if the player has escaped?
+    }
+
     IEnumerator wait()
     {
         while (true)
         {
-            Vector3 pos = Random.onUnitSphere * radius; //picks a random point on the surface of a sphere with the radius
+            if (!alerted && !moving)
+            {
+                pos = Random.onUnitSphere * radius; //picks a random point on the surface of a sphere with the radius
+                transform.LookAt(pos, transform.position * Time.deltaTime); //makes them face the direction they are moving to
+            }
             yield return new WaitForSeconds(Random.Range(MINWAIT, MAXWAIT)); //pause for a random time and then go again
         }
     }
@@ -68,16 +90,18 @@ public class AnimalController : MonoBehaviour
     //Moving towards the current position
     private void Movement()
     {
+        moving = false;
         if (
-            Vector3.Distance(transform.position, pos) > 1 //if the animal is not at the desired position
-            && //If any of the following three statements are false it returns true. If all are true it returns false. This is a manual way of making a NAND gate
-            (
-                !Physics.Raycast(transform.position, pos, out RaycastHit hit, .5f)
-                || !hit.collider.gameObject.tag == "OnPlanetCollision"
-                || !hit.collider.gameObject.transform.parent.gameObject != gameObject
-            ) //If the animal sees a collider that is for collision and not itself
+            Vector3.Distance(transform.position, pos) > 2 //if the animal is not at the desired position
+            // && //If any of the following three statements are false it returns true. If all are true it returns false. This is a manual way of making a NAND gate
+            // (
+            //     !Physics.Raycast(transform.position, pos, out RaycastHit hit, .5f)
+            //     || hit.collider.gameObject.tag != "OnPlanetCollision"
+            //     || hit.collider.gameObject.transform.parent.gameObject == gameObject
+            // ) //If the animal sees a collider that is for collision and not itself
         )
         {
+            moving = true;
             transform.position = Vector3.MoveTowards(
                 transform.position,
                 pos,
