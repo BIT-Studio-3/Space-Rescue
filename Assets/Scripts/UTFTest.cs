9162fe268d0fefdbc5b290d8db6ff6c724ac8a19
Erika Stuart <erikat.stuart@gmail.com>
2023-08-27 20:23:03 +1200

    FUnctional tests, not important tests but they work

---
+++ Assets/Scripts/UTFTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UTFTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int count = 0;
+
+    public void Increment(){
+        count += 1;
+    }
+}
