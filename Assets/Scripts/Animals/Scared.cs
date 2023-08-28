a71f56f7f0e4762daf1c480016173264d7c037c0
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-08-28 20:04:36 +1200

    Scared animal exclamation point now lasts for 10 seconds

--- Assets/Scripts/Animals/Scared.cs
+++ Assets/Scripts/Animals/Scared.cs
@@ -2,7 +2,7 @@
 // Created by: Palin Wiseman
 // Created on: 23/08/2023
 // Last modified by: Palin Wiseman
-// Last modified on: 23/08/2023
+// Last modified on: 28/08/2023
 
 using System.Collections;
 using System.Collections.Generic;
@@ -17,6 +17,8 @@ public class Scared : MonoBehaviour
     private GameObject player;
 
     private bool alerted;
+    private float timeStart;
+    private const float WAITTIME = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -33,8 +35,9 @@ public class Scared : MonoBehaviour
         {
             animalController.Scared(player.transform.position);
             warningSymbol.SetActive(true);
+            timeStart = Time.time; //Sets the time that the animal was alerted to the current time
         }
-        else if (warningSymbol.activeSelf) //If they are not alerted but the warning symbol is on then turn it off
+        else if (warningSymbol.activeSelf && timeStart + WAITTIME <= Time.time) //If they are not alerted but the warning symbol is on and it has been wait time seconds since alerted then turn it off
         {
             warningSymbol.SetActive(false);
         } //If the animal is not alerted and the symbol isn't on then do nothing
