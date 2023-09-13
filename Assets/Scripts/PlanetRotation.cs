cd3dae3e588719b310e49a2d56455bf000c5bda5
Yousef Altay (1000070390) <ALTAYY1@student.op.ac.nz>
2023-09-14 11:13:49 +1200

    player rotation on planet is enabled without clicking

--- Assets/Scripts/PlanetRotation.cs
+++ Assets/Scripts/PlanetRotation.cs
@@ -9,22 +9,25 @@ public class PlanetRotation : MonoBehaviour
     public float sensitivityY = 2.0f; // Mouse sensitivity for vertical
     private float rotationX = 0.0f;  // Current X rotation of the camera.
 
+    void Start()
+    {
+        // Set the initial camera rotation to look at the player.
+        Vector3 lookAtPlayerDirection = player.position - transform.position;
+        Quaternion initialRotation = Quaternion.LookRotation(lookAtPlayerDirection);
+        transform.rotation = initialRotation;
+    }
     void Update()
     {
-        // Check for right mouse button click.
-        if (Input.GetMouseButton(1))
-        {
-            // mouse input.
-            float mouseX = Input.GetAxis("Mouse X") * sensitivityX;
-            float mouseY = Input.GetAxis("Mouse Y") * sensitivityY;
+        // mouse input.
+        float mouseX = Input.GetAxis("Mouse X") * sensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivityY;
 
-            // player's rotation around the Y-axis
-            player.Rotate(Vector3.up * mouseX);
+        // player's rotation around the Y-axis
+        player.Rotate(Vector3.up * mouseX);
 
-            // camera's rotation around the X-axis
-            rotationX -= mouseY;
-            rotationX = Mathf.Clamp(rotationX, -90f, 90f);
-            Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        }
+        // camera's rotation around the X-axis
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
     }
 }
