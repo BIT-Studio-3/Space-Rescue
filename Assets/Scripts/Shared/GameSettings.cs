a6bd3785b16151bad8d7a302b8e2ae7efeb80585
Palin Wiseman <wisepb1@student.op.ac.nz>
2023-09-18 11:11:09 +1200

    Added music and sound effects global variable

--- Assets/Scripts/Shared/GameSettings.cs
+++ Assets/Scripts/Shared/GameSettings.cs
@@ -6,17 +6,23 @@ public static class GameSettings
 {
     //Global variable to adjust game mechanics in tutorial
     public static bool Tutorial = false;
+
     //Global variable to keep track of amount of rescued animals
     public static int Score = 0;
+
     //Variable to see if player has lost
     public static bool Winning = true;
+
     //Variable to see how many planets are in danger
     //Need to add -- to this variable when planet is saved
     public static int PlanetDanger = 0;
 
     //Global Array that will be populated with the variety of planet prefabs in the resources folder
     public static GameObject[] planetPrefabs;
-    
 
+    //Volume from 0 to 1 for the music
+    public static float MusicVolume = .5f;
+
+    //Volume from 0 to 1 for the sound effects
+    public static float SoundEffectsVolume = .5f;
 }
-
