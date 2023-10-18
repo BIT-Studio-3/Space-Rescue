91a39bb4b2a319ba192495fc005ca40f4053843b
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-10-19 11:04:35 +1300

    Player cannot move while in death state

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public enum PlayerState //Enum of possible states for the player
 {
     IDLE,
@@ -12,6 +13,7 @@ public enum PlayerState //Enum of possible states for the player
     PICKUP,
     DEATH
 }
+
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
@@ -24,8 +26,11 @@ public class PlayerMovement : MonoBehaviour
     private Animator animator;
     private PlayerState animationState;
 
-    public PlayerState AnimationState { get => animationState; set => animationState = value; }
-
+    public PlayerState AnimationState
+    {
+        get => animationState;
+        set => animationState = value;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -38,36 +43,37 @@ public class PlayerMovement : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         animator = GameObject.Find("Model").GetComponent<Animator>();
-
     }
 
     void Update()
     {
-        isMoving = (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
-        direction = new Vector3(
-            Input.GetAxisRaw("Horizontal"),
-            0,
-            Input.GetAxisRaw("Vertical")
-        ).normalized;
-        Vector3 targetMove = direction * speed;
-        //current, target, velocity,  smooth time
-        moveAmo = Vector3.SmoothDamp(moveAmo, targetMove, ref smoothMoveVel, .1f);
-        if (isMoving)
+        if (AnimationState != PlayerState.DEATH)
         {
-            AnimationState = PlayerState.RUN;
-        }
-        else
-        {
-            AnimationState = PlayerState.IDLE;
-        }
+            isMoving = (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0);
+            direction = new Vector3(
+                Input.GetAxisRaw("Horizontal"),
+                0,
+                Input.GetAxisRaw("Vertical")
+            ).normalized;
+            Vector3 targetMove = direction * speed;
+            //current, target, velocity,  smooth time
+            moveAmo = Vector3.SmoothDamp(moveAmo, targetMove, ref smoothMoveVel, .1f);
+            if (isMoving)
+            {
+                AnimationState = PlayerState.RUN;
+            }
+            else
+            {
+                AnimationState = PlayerState.IDLE;
+            }
 
-        if (Input.GetKeyDown(Keybinds.Interact))
-        {
-            AnimationState = PlayerState.PICKUP;
+            if (Input.GetKeyDown(Keybinds.Interact))
+            {
+                AnimationState = PlayerState.PICKUP;
+            }
+
+            PlayAnimation();
         }
-
-        PlayAnimation();
-
     }
 
     void FixedUpdate()
@@ -81,14 +87,17 @@ public class PlayerMovement : MonoBehaviour
             transform.GetChild(0).localRotation = Quaternion.LookRotation(direction);
         }
         //The reason I am using raycasts here and not just non trigger colliders is because the player can use the animals as a ramp with proper colliders
-        if (Physics.Raycast(transform.GetChild(0).position, localMove, out hit, .5f)
-            && hit.collider.gameObject.tag == "OnPlanetCollision")
+        if (
+            Physics.Raycast(transform.GetChild(0).position, localMove, out hit, .5f)
+            && hit.collider.gameObject.tag == "OnPlanetCollision"
+        )
         {
             return;
         }
         //If there is no trigger collider in the movement direction (or it is the collider for the animal FOV) the player moves
         rb.MovePosition(rb.position + localMove);
     }
+
     private void PlayAnimation() //Plays the current animation State given from the enum
     {
         if (animator != null)
@@ -105,8 +114,7 @@ public class PlayerMovement : MonoBehaviour
                 case PlayerState.DEATH:
                     animator.Play("Death");
                     break;
-                    //I dont include Idle because the other animations are directed to play idle immediately after they are done
-
+                //I dont include Idle because the other animations are directed to play idle immediately after they are done
             }
         }
     }
